a4104d7b3e6dfe20815e7ffb8795f2776f12298e
2020005 <u2020005@giki.edu.pk>
2023-12-01 17:45:02 +0500

    added files

---
+++ build/precache-manifest.3eb2293a522b533f781f3eea14d4dfd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ef292ee3e536da1327c0289f49e18cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49d4c9276c8ae6ab5563",
+    "url": "/static/css/main.0fe2e49c.chunk.css"
+  },
+  {
+    "revision": "540d322bbfe8279636f6",
+    "url": "/static/js/2.3eaeb412.chunk.js"
+  },
+  {
+    "revision": "3243835478fc6efa88cf",
+    "url": "/static/js/3.3a4c2a80.chunk.js"
+  },
+  {
+    "revision": "49d4c9276c8ae6ab5563",
+    "url": "/static/js/main.39ce6e21.chunk.js"
+  },
+  {
+    "revision": "2f6fb313fb50bb51611a",
+    "url": "/static/js/runtime~main.074387dd.js"
+  }
+]);
